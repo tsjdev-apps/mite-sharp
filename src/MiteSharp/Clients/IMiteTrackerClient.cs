fafeb9ce2947560ad448c3037f4ffd6063f69fa9
Sebastian Jensen <apps@tsjdev-apps.de>
2023-11-01 08:34:26 +0100

    Add MiteTrackerService.

---
+++ src/MiteSharp/Clients/IMiteTrackerClient.cs
@@ -0,0 +1,42 @@
+﻿using MiteSharp.Models.Mite;
+using Refit;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MiteSharp.Clients
+{
+    /// <summary>
+    /// Interface for Mite Tracker Client
+    /// </summary>
+    internal interface IMiteTrackerClient
+    {
+        /// <summary>
+        /// Gets the tracker asynchronously
+        /// </summary>
+        /// <param name="miteApiKey">The Mite API key</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>The Mite Tracker Root Object</returns>
+        [Get("/tracker.json")]
+        Task<MiteTrackerRootObject> GetTrackerAsync([Header("X-MiteApiKey")] string miteApiKey, CancellationToken ct);
+
+        /// <summary>
+        /// Starts the tracker asynchronously
+        /// </summary>
+        /// <param name="miteApiKey">The Mite API key</param>
+        /// <param name="timeEntryId">The time entry ID</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>The Mite Tracker Root Object</returns>
+        [Patch("/tracker/{timeEntryId}.json")]
+        Task<MiteTrackerRootObject> StartTrackerAsync([Header("X-MiteApiKey")] string miteApiKey, int timeEntryId, CancellationToken ct);
+
+        /// <summary>
+        /// Stops the tracker asynchronously
+        /// </summary>
+        /// <param name="miteApiKey">The Mite API key</param>
+        /// <param name="timeEntryId">The time entry ID</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>The Mite Tracker Root Object</returns>
+        [Delete("/tracker/{timeEntryId}.json")]
+        Task<MiteTrackerRootObject> StopTrackerAsync([Header("X-MiteApiKey")] string miteApiKey, int timeEntryId, CancellationToken ct);
+    }
+}
---
+++ src/MiteSharp/Models/TrackingTimeEntry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace MiteSharp.Models
+{
+    /// <summary>
+    ///     Represents a tracking time entry in Mite
+    /// </summary>
+    public class TrackingTimeEntry
+    {
+        /// <summary>
+        ///     The ID of the time entry
+        /// </summary>
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        /// <summary>
+        ///     The number of minutes for the stopped time entry
+        /// </summary>
+        [JsonPropertyName("minutes")]
+        public int Minutes { get; set; }
+
+        /// <summary>
+        ///     Indicates the date and time since the tracker is running
+        /// </summary>
+        [JsonPropertyName("since")]
+        public DateTime Since { get; set; }
+    }
+
+}
